a8232a3ce38062185b78b535c4d449d3ff0d0324
闫家豪 <1461766338@qq.com>
2026-09-10 21:20:18 +0800

    deploy: 配置 GitHub Pages 自动部署,适配 base 路径

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://wbxxmz.github.io',
+  base: '/my-boke',
 });
